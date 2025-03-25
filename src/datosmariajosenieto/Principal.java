3b3b29aa66074027cb2d2dce5ef147518125b8fb
Maria Jose Nieto <mariaj.nietorobledo@gmail.com>
2025-03-25 22:36:47 +0100

    Versión inicial

---
+++ src/datosmariajosenieto/Principal.java
@@ -0,0 +1,10 @@
+package datosmariajosenieto;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Maria Jose Nieto Robledo");
+	}
+
+}
